c6ba25ba9610656b3de9818cfd78468d2a6afa09
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2024-04-03 23:37:35 +0100

    Adds CSS and assets to webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import './styles.css'
+import IMAGE from './img.webp'
+
 export default function App() {
   return (
-    <h1>React Typescript Webpack Starter Template</h1>
+    <>
+        <h1>React Typescript Webpack Starter Template</h1>
+        <img src={IMAGE} alt="Pallet" width="300" height="200" />
+    </>
   )
 }
